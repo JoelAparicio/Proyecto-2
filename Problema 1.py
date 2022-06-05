946416a355fa18b40df92e803ba2486146da39f2
JoelAparicio <joelaparicio454@gmail.com>
2022-06-05 08:09:49 -0500

    Se completo el problema 1
    Se eliminaron los archivos de prueba ya que no serán útiles en un futuro.

--- Problema 1.py
+++ Problema 1.py
@@ -1,3 +1,6 @@
+from matplotlib import pyplot
+
+
 def regula_falsi(funcion, x_a, x_b, iteraciones=10, error_r=0.01):
     # Se inicializan las variables
     solucion = None
@@ -12,7 +15,7 @@ def regula_falsi(funcion, x_a, x_b, iteraciones=10, error_r=0.01):
             error_calculado = abs((solucion - x_a) / solucion) * 100
             # Se redefine el nuevo intervalo con los signos
             if funcion(x_a) * funcion(solucion) >= 0:  # Si la funcion evaluada en el punto a por la funcion evaluada
-                                                       # en la solucion es mayor o igual a 0
+                # en la solucion es mayor o igual a 0
                 x_a = solucion
             else:
                 x_b = solucion
@@ -24,4 +27,45 @@ def regula_falsi(funcion, x_a, x_b, iteraciones=10, error_r=0.01):
         print('no existe solucion en ese intervalo propuesto')
 
 
-regula_falsi(lambda x: -0.5 * (x ** 2) - 2.5 * x + 4.5, -1.5, 2, 3, 1)
+# Función cuadrática.
+def f1(x):
+    return -0.5 * (x ** 2) + 2.5 * x + 4.5
+
+
+# Función lineal que corta la funcion cuadratica.
+def f2(x):
+    return 2 * x + 0
+
+
+# Valores del eje X que toma el gráfico.
+x = range(-10, 15)
+# Graficar ambas funciones.
+pyplot.plot(x, [f1(i) for i in x])
+pyplot.plot(x, [f2(i) for i in x])
+# Establecer el color de los ejes.
+pyplot.axhline(0, color="black")
+pyplot.axvline(0, color="black")
+# Limitar los valores de los ejes.
+pyplot.xlim(-15, 15)
+pyplot.ylim(-15, 15)
+# Mostrar la grafica.
+pyplot.show()
+
+print("Una vez vista la grafica, ingrese la ecuacion -0.5x^2+2.5x+4.5\n")
+ecuacion = a, b, c = [float(input(f'Ingresa el coeficiente {coef}: ')) for coef in ('a', 'b', 'c')]
+while a != 0.5 and b != 2.5 and c != 4.5:
+    print("No ingreso los coeficientes indicados en la ecuacion -0.5x^2+2.5x+4.5 \n")
+    ecuacion = a, b, c = [float(input(f'Ingresa el coeficiente {coef}: ')) for coef in ('a', 'b', 'c')]
+primer_punto = float(input("ingrese el primer punto en la grafica: "))
+segundo_punto = float(input("ingrese el segundo punto en la grafica: "))
+iteracion = int(input("Ingrese las iteraciones deseadas: "))
+while iteracion <= 0:
+    print("Las iteraciones ingresadas deben ser mayores a 0\n")
+    iteracion = int(input("Ingrese las iteraciones deseadas: "))
+error = int(input("Ingrese el porcentaje de error deseado: "))
+while error <= 0:
+    print("El error deseado no puede ser negativo")
+    error = int(input("Ingrese el porcentaje de error deseado: "))
+print("\nRegula-Falsi:\n")
+
+regula_falsi(lambda x: -a * (x ** 2) - b * x + c, primer_punto, segundo_punto, iteracion, error)
